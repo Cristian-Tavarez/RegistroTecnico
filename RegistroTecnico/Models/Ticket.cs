f4f582519182abd81dcff4a135eeb3c6d5c888ab
cristian tavarez <cristian22041@gmail.com>
2025-02-02 15:06:53 -0400

    clase ticket

---
+++ RegistroTecnico/Models/Ticket.cs
@@ -0,0 +1,11 @@
+﻿public class Ticket
+{
+    public int TicketId { get; set; }
+    public DateTime Fecha { get; set; }
+    public string Prioridad { get; set; }
+    public int ClienteId { get; set; }
+    public string Asunto { get; set; }
+    public string Descripcion { get; set; }
+    public int TiempoInvertido { get; set; }
+    public int TecnicoId { get; set; }
+}
